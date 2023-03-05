3d5139efcd1964932f0ac37dd3dd3fb474e1062a
usmansalim3 <usmansalim3@gmail.com>
2023-03-05 10:12:41 +0530

    usman passed adarsh rocked

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -38,7 +38,7 @@ const CheckOut = () =>{
       <div className="w-3/4 bg-white px-10 py-10">
         <div className="flex justify-between border-b pb-8">
           <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-          <h2 className="font-semibold text-2xl">3 Items</h2>
+          <h2 className="font-semibold text-2xl">{cart.length} Items</h2>
         </div>
         <div className="flex mt-10 mb-5">
           <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
@@ -56,26 +56,26 @@ const CheckOut = () =>{
       </div>
 
       <div id="summary" className="w-1/4 px-8 py-10">
-        <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
+        <h1 className="font-semibold text-2xl border-b pb-8 text-white">Order Summary</h1>
         <div className="flex justify-between mt-10 mb-5">
-          <span className="font-semibold text-sm uppercase">Items 3</span>
-          <span className="font-semibold text-sm">{totalCostCart}$</span>
+          <span className="font-semibold text-sm uppercase text-white">Items {cart.length}</span>
+          <span className="font-semibold text-sm text-white">{totalCostCart}$</span>
         </div>
         <div>
-          <label className="font-medium inline-block mb-3 text-sm uppercase">Shipping</label>
+          <label className="font-medium inline-block mb-3 text-sm uppercase text-white">Shipping</label>
           <select className="block p-2 text-gray-600 w-full text-sm">
             <option>Standard shipping - $10.00</option>
           </select>
         </div>
         <div className="py-10">
-          <label for="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Promo Code</label>
+          <label for="promo" className="font-semibold inline-block mb-3 text-sm uppercase text-white">Promo Code</label>
           <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full"/>
         </div>
-        <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Apply</button>
+        <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase text-white">Apply</button>
         <div className="border-t mt-8">
           <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-            <span>Total cost</span>
-            <span>{totalCostCart}</span>
+            <span className="text-white">Total cost</span>
+            <span className="text-white">{totalCostCart+10}</span>
           </div>
           <button onClick={checkOutContract} className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
         </div>
